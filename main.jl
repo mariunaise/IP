80248616032ef26e2ca4b14302ba893fbef21888
Marius Drechsler <marius@nixfunktioniert.de>
2024-11-19 18:08:51 +0100

    Basic workflow setup

--- main.jl
+++ main.jl
@@ -3,17 +3,29 @@ Pkg.activate("env")
 
 using Random
 using Distributions
+ENV["GDK_BACKEND"] = "x11"
+using Gadfly
 
 include("julia_code/bach.jl")
+include("julia_code/helperfunctions.jl")
 
 function main()
+
+    # Define the parameters of the input probability distribution
     mean = 0
     std_dev = 1
+
+    # Amount of addends in the linear combination
+    n = 3
+
+    # Define the number of bits we want to extract
+    m = 2
 
     dist = Normal(mean, std_dev)
     data = rand(dist, 1000000)
 
-    bach(data)
+    # Call the BACH function to do the magic
+    bach( data,n, m)
 end
 
 main()
