680de6590ba3ee989b1b99549621781b7f61043f
Marius Drechsler <marius@nixfunktioniert.de>
2024-11-19 18:08:39 +0100

    Add the create_linearcombinations function as function

--- julia_code/helperfunctions.jl
+++ julia_code/helperfunctions.jl
@@ -3,8 +3,26 @@ function generate_n_bit_numbers_alpha(n, a)
     numbers = []
     for i in 0:(2^n - 1)
         binary_str = bitstring(i)[end-n+1:end]
-        transformed = [c == '0' ? a : 1 for c in binary_str]
+        transformed = [c == '0' ? a : -a for c in binary_str]
         push!(numbers, transformed)
     end
     return numbers
+end
+
+function create_linearcombinations(inputs, weights, n) 
+    collect(
+    map(
+        set -> begin
+            LinearCombinationSet(
+                collect(map(
+                    weights -> begin
+                        LinearCombination(weights, set, sum(weights .* set))
+                    end,
+                    weights
+                ))
+            )
+        end, 
+        Iterators.partition(inputs, n)
+    )
+)
 end
